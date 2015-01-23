c546b1a3dd64ef64c3f5281c8d8ae6458c852db7
Merhat <merhat1998@gmail.com>
2015-01-23 23:54:45 +0200

    Homework

---
+++ SecHomeworkC#/FibonacciNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int  n = int.Parse(Console.ReadLine());
+          int[] s = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 };
+           int k = 0;
+           while (s[n] != s[k])
+           {
+               Console.Write(s[k]);
+               k++;
+           }
+           Console.WriteLine();
+          
+            
+
+        }
+    }
+}
---
+++ SecHomeworkC#/NumbersinIntervalDividableByGivenNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int p = 0;
+            int i = start;
+            int a = 1;
+            int[] s = new int[a];  
+            
+          
+            while (end >= i) 
+            {
+                
+                if (i % 5 == 0) 
+                {
+                    p++;
+                    
+                    
+                }
+                i++;
+                
+            }
+            Console.WriteLine("Numbers are  {0}",p);
+            
+            
+
+
+        }
+    }
+}
