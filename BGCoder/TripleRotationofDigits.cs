2e39240b72a40323b1d3df4e1c36e3162ed875f7
Merhat <merhat1998@gmail.com>
2015-02-02 23:35:13 +0200

    SomeBGCODER

---
+++ BGCoder/TripleRotationofDigits.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long k = long.Parse(Console.ReadLine());
+            long m = k;
+            if (k < 0)
+            {
+                k = -k;
+            }
+            string z = Convert.ToString(k);
+            long s = 0;
+            long d = 0;
+            long l = 0;
+            s = k % 10;
+            k = k / 10;
+            d = k % 10;
+            k = k / 10;
+            long f = k;
+            l = k % 10;
+            k = k / 10;
+
+            if (z.Length == 3)
+            {
+                if (s != 0)
+                {
+                    Console.WriteLine(z);
+                }
+                else if (s == 0 && d != 0) 
+                {
+                    Console.WriteLine("{0}{1}",d,f);
+                }
+                else if (s == 0 && d == 0) 
+                {
+                    Console.WriteLine(l);
+                }
+            }
+            else if (z.Length == 2)
+            {
+                Console.WriteLine("{0}{1}", s, d);
+            }
+            else if (z.Length == 1)
+            {
+                Console.WriteLine(s);
+            }
+            else
+            {
+                if (l != 0)
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", l, d, s, k);
+                }
+                if (l == 0 && d != 0)
+                {
+                    Console.WriteLine("{1}{2}{3}", d, s, k);
+                }
+                if (l == 0 && d == 0 && s != 0)
+                {
+                    Console.WriteLine("{0}{1}", s, k);
+                }
+                if (l == 0 && d == 0 && s == 0)
+                {
+                    Console.WriteLine("{0}", k);
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ BGCoder/UKFlag.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char dot = '.';
+            char left = '/';
+            char star = '*';
+            char right = '\\';
+            char up = '|';
+            char dash = '-';
+
+            //top
+            for (int i = 0; i <= n / 2; i++)
+            {
+                for (int j = 0; j <n / 2; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(right);
+                    }
+                    else if (i != n / 2)
+                    {
+                        Console.Write(dot);
+                    }
+                }
+                if (i != n / 2)
+                {
+                    Console.Write(up);
+                }
+                for (int s = n/2; s > 0; s--)
+                {
+                    if (i == s-1)
+                    {
+                        Console.Write(left);
+                    }
+                    else if (i != n / 2)
+                    {
+                        Console.Write(dot);
+                    }
+                }
+
+
+                if (i != n / 2)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            //Middle
+            Console.Write(new string(dash, n / 2));
+            Console.Write(new string(star, 1));
+            Console.Write(new string(dash, n / 2));
+            Console.WriteLine();
+
+            //Bottom
+
+
+            for (int i = 0; i <= n/2; i++)
+            {
+                for (int s = n / 2; s > 0; s--)
+                {
+                    if (i == s - 1)
+                    {
+                        Console.Write(left);
+                    }
+                    else if (i != n / 2)
+                    {
+                        Console.Write(dot);
+                    }
+                   
+                }
+                if (i != n / 2)
+                {
+                    Console.Write(up);
+                }
+                for (int j = 0; j < n / 2; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(right);
+                    }
+                    else if (i != n / 2)
+                    {
+                        Console.Write(dot);
+                    }
+                }
+                
+
+
+                if (i != n / 2)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
